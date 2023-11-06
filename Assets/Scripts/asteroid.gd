a478545e330cb3d9b7751efa021b1b64756bf07a
Kalto-Mate <Guillermo_Gonzalez_Sanchez@outlook.com>
2023-11-06 21:13:18 +0100

    Code cleanup, actually apply velocity

--- Assets/Scripts/asteroid.gd
+++ Assets/Scripts/asteroid.gd
@@ -3,7 +3,7 @@ extends RigidBody2D
 @export var Visuals: Sprite2D
 @export var Collision: CollisionShape2D
 
-var x_direction:int = -1
+@export var local_movement_direction : Vector2 = Vector2(-1,0)
 @export var min_vel:int = 4
 @export var max_vel:int = 8
 var vel_multiplier:float = 50
@@ -17,27 +17,37 @@ var angularvel_multiplier:float = 500
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	
-	#Randomize Linear Speed
+	#Randomize Linear Speed ====================================================================
 	var velocity:float = randf_range(min_vel, max_vel) * vel_multiplier
 	
-	#Make it into a vector to higlight the transformation process, useful for non-pure directions
-	var direction : Vector2 = Vector2(-1,0) #Or Vector2.LEFT
-	var velocity_vector : Vector2 = direction * velocity
-	self.linear_velocity = self.get_global_transform().basis_xform(velocity_vector)
-	#Transform cannot be grabbed to reference directly, so we use a getter
+	#Transform cannot be grabbed to reference directly, so we use a getter:
+	var local_transform : Transform2D = self.get_transform()
 	
-	#Randomize Spin
+	#self.linear_velocity = self.local_transform.basis_xform(velocity_vector)
+	
+		#while the above is perfectly servizable with a 1,1 scale, it can break if the scale is 
+		# anything else. this happens because the scale makes the basis part of the Transform 2x3
+		# structure to not be of magnitude 1
+	
+		#the answer is to normalise the adapted local direction into the global reference system
+		#which turns it into an identity, magnitude 1 vector, and restore the velocity by
+		#multiplicating it by the absolute velocity value
+		
+	var global_velocity: Vector2 = local_transform.basis_xform(local_movement_direction).normalized() * velocity
+	self.linear_velocity = global_velocity
+	
+	#Randomize Spin====================================================================
 	var angular_vel:float = randi_range(min_angular_vel, max_angular_vel)
 	var rotation_direction: float = pow(-1, randi_range(0,1))
 	angular_vel = angular_vel * angularvel_multiplier * rotation_direction
 	self.constant_torque = angular_vel
 	
-	#Randomize Size
+	#Randomize Size====================================================================
 	var resize_factor:float = randf_range(0.5, 1.5)
 	Visuals.scale = Visuals.scale * resize_factor
 	Collision.scale = Collision.scale * resize_factor
 	
-	#Randomize Looks
+	#Randomize Looks====================================================================
 	var skinIndex : int = randi() % Skins.size()
 	var Chosen_Skin : Texture2D = Skins[skinIndex]
 	Visuals.set_texture(Chosen_Skin)
